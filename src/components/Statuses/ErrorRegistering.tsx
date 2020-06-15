c4e144edfa0de8ccf7136c16da20723d13e07e5b
Chintu Mohandas <chintu.mohandas@gmail.com>
2020-06-15 22:30:35 +0200

    src removed

--- src/components/Statuses/ErrorRegistering.tsx
+++
@@ -1,7 +0,0 @@
-import { Result } from 'antd';
-import React from 'react';
-
-const ErrorRegistering: React.FC<{ message: string | null }> = ({ message }) => {
-  return <Result status="error" title={message || 'Failed to register, please try again later!'} />;
-};
-export default ErrorRegistering;
--- src/contentful/constants.ts
+++
@@ -1,4 +0,0 @@
-export enum LOCALES {
-    "ml-IN" = 'ml-IN',
-    "en-US" = 'en-US',
-}
