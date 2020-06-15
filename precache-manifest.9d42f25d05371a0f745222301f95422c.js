0530a089a15549691a7321e7cc99a45b4c86bd59
SimiS22 <simiswaminathan@gmail.com>
2020-06-15 17:27:52 +0200

    Updates

---
+++ precache-manifest.9d42f25d05371a0f745222301f95422c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e00d474fc7ccfe633cdee9c9c7d60d",
+    "url": "/kerala-samajam/index.html"
+  },
+  {
+    "revision": "dd453569925422a8ee15",
+    "url": "/kerala-samajam/static/css/main.e391308d.chunk.css"
+  },
+  {
+    "revision": "74446c230267efbd8da2",
+    "url": "/kerala-samajam/static/js/2.a7b6b48f.chunk.js"
+  },
+  {
+    "revision": "f576af3254df0679363d3e6ffe804e36",
+    "url": "/kerala-samajam/static/js/2.a7b6b48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd453569925422a8ee15",
+    "url": "/kerala-samajam/static/js/main.ed3336d7.chunk.js"
+  },
+  {
+    "revision": "eda37a25a90a897a9dee",
+    "url": "/kerala-samajam/static/js/runtime-main.b6b748db.js"
+  },
+  {
+    "revision": "b95d8e304350804e77e5284c6dc68d32",
+    "url": "/kerala-samajam/static/media/ksh.b95d8e30.png"
+  },
+  {
+    "revision": "6117f73b1f58a69710f73dad751e33a4",
+    "url": "/kerala-samajam/static/media/news-announcement.6117f73b.jpg"
+  }
+]);
