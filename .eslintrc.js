bc1be8576f5d76477cb766394740e956e328f9d8
SimiS22 <simiswaminathan@gmail.com>
2020-12-28 14:48:42 +0100

    linting and formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,23 +4,31 @@ module.exports = {
     es2021: true
   },
   extends: [
-    'plugin:react/recommended',
-    'standard'
+    "plugin:react/recommended"
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true
     },
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: "module"
   },
   plugins: [
-    'react',
-    '@typescript-eslint'
+    "react",
+    "@typescript-eslint"
   ],
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
   rules: {
-    "semi": ["error", "always"],
-    "quotes": ["error", "double"]
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    "react/prop-types" : "off",
+    "react/no-unescaped-entities":"off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error"]
   }
-}
+};
