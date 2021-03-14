51f769f6222733b4ba6e831b91047280433b36df
Simi Swaminathan <simi.swaminathan@AERQ-MBP-11.local>
2021-03-14 23:10:49 +0100

    added functionality to cookie consent bar

---
+++ public/firbase-init.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBkXpWASfQLkLq90COfr_vokTHRyZ-Cxm8",
+  authDomain: "kerala-samajam.firebaseapp.com",
+  databaseURL: "https://kerala-samajam.firebaseio.com",
+  projectId: "kerala-samajam",
+  storageBucket: "kerala-samajam.appspot.com",
+  messagingSenderId: "423784195595",
+  appId: "1:423784195595:web:295d7a52d51d602015090e",
+  measurementId: "G-X6W4TG277C"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
